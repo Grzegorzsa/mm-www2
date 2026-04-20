f8bf29d63cf1c1a0ac7b9a83c0aad999cc82a533
Grzegorzsa <Grzegorzsa@users.noreply.github.com>
2026-04-20 19:45:11 +0200

    feat: add allowedDevOrigins for development environment configuration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['mxbeats.com', 'http://localhost:3000'],
   images: {
     localPatterns: [
       { pathname: '/api/media/file/**' },
